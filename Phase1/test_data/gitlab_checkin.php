b8612c9223718bbe039edb25bd5bb4888a711ff0
Alan Gonzalez <alandgonzalez@gmail.com>
2021-11-22 01:38:54 -0500

    Update gitlab_checkin.php

--- Phase1/test_data/gitlab_checkin.php
+++ Phase1/test_data/gitlab_checkin.php
@@ -1,2 +1,2 @@
 gitlab_key=glpat-zsDLpzC_so28Qnzvqcrh
-url = https://oauth2:1AbCDeF_g2HIJKLMNOPqr@gitlab.com/yourusername/project.git 
+url = https://oauth2:1AbCDeF_g2HIJKLMNOPqr@gitlab.com/yourusername/project.git
